ca5c8a5a812e3e78f95901c5acc09df0d1cfac59
Daniel Hindrikes <daniel@hindrikes.se>
2022-11-22 22:33:04 +0100

    secret keys

---
+++ ServiceBusManager/SecretKeys.github.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ServiceBusManager;
+
+public static class SecretKeys
+{
+    public const string PremiumKey = "{PremiumKey}";
+    public const string AppCenterProd = "";
+    public const string AppCenterDev = "";
+}
+
